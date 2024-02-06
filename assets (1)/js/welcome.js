6c656b2306bf8ce14e3d95b375d6c6e25e52d8e1
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-02-06 17:34:26 +0100

    update

--- assets (1)/js/welcome.js
+++ assets (1)/js/welcome.js
@@ -4,11 +4,33 @@
 function checkFunction (){
     const checkBox = document.getElementById("check");
     const btn = document.getElementById("btn-change");
+    
+    if(checkBox.checked){
+            btn.innerText ="";
+            btn.classList.add("button-shining"); // comando per accendere il botone
 
-        if(checkBox.checked){
-            btn.classList.add("button-shining")
-        }else{
-            btn.classList.remove("button-shining")
-        }
+            const creatTag = document.createElement("a"); // Creare il elemento
+            creatTag.setAttribute("href","./benchmark.html") // Metere un atributo all'elemento <a>
+            
+            const textButton = document.createTextNode("PROCEED") // Creare un testo nel botone con il link
+            creatTag.appendChild(textButton); // collegare il figlio testo all'elemento <a>
+            
+            const button = document.getElementById("btn-change");
+            button.appendChild(creatTag);
+
+           
+        }else {
+
+            btn.classList.remove("button-shining");
+                    
+            const aElement = document.querySelector("a");
+
+            if(aElement){
+                btn.removeChild(aElement);
+                btn.innerText = "PROCEED";
+            };
+        };
+          
 }; 
+
 
